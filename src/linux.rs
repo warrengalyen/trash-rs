cf1798afb76bd7dbd074926e6aa2baed9b9bd060
Warren Galyen <wgalyen@gmail.com>
2020-10-12 17:57:25 -0700

    Fixed platform specific compilation

--- src/linux.rs
+++ src/linux.rs
@@ -1,5 +1,9 @@
+use std::path::Path;
+
+use crate::Error;
+
 pub fn is_implemented() -> bool {
-    false
+    true
 }
 
 pub fn platform_remove<T: AsRef<Path>>(path: T) -> Result<(), Error> {
--- src/macos.rs
+++ src/macos.rs
@@ -1,3 +1,7 @@
+use std::path::Path;
+
+use crate::Error;
+
 pub fn is_implemented() -> bool {
     false
 }
